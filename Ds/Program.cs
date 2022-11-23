c13e388ce2447495e11c037a18e286fb49e3895b
Dawid Tomaszewski <dawiidtomaszewski354@gmail.com>
2022-11-23 15:14:09 +0100

    init 2

--- Ds/Program.cs
+++ Ds/Program.cs
@@ -5,6 +5,10 @@ using System.Data.SqlClient;
 using MySql.Data.MySqlClient;
 using System.Threading;
 using System.Collections.Generic;
+using Microsoft.VisualBasic;
+using Org.BouncyCastle.Utilities.Collections;
+using System.Security.Policy;
+using Org.BouncyCastle.Crypto.Modes.Gcm;
 
 namespace ConsoleApp1
 {
@@ -23,6 +27,45 @@ namespace ConsoleApp1
             con.Open();
 
 
+            string getnick(string login, string password)
+            {
+                string que = "select nick from users WHERE login ='" + login + "' AND haslo ='" + password + "'";
+                MySqlCommand gg = new MySqlCommand(que, con);
+                return (string)gg.ExecuteScalar();
+            }
+            void setnick(string login, string password,string nick)
+            {
+                string que = "UPDATE `users` SET `nick`= '" + nick + "' WHERE `login`= '" + login + "'AND`haslo`= '" + password + "'; ";
+                MySqlCommand ggg = new MySqlCommand();
+                ggg.CommandText = que;
+                ggg.Connection = con;
+                ggg.ExecuteNonQuery();
+            }
+
+
+
+            string gettag(string login, string password)
+            {
+                string que = "select tag from users WHERE login ='" + login + "' AND haslo ='" + password + "'";
+                MySqlCommand gg = new MySqlCommand(que, con);
+                return (string)gg.ExecuteScalar();
+            }
+            void settag(string login, string password, string tag)
+            {
+                string que = "UPDATE `users` SET `tag`= '" + tag + "' WHERE `login`= '" + login + "'AND`haslo`= '" + password + "'; ";
+                MySqlCommand ggg = new MySqlCommand();
+                ggg.CommandText = que;
+                ggg.Connection = con;
+                ggg.ExecuteNonQuery();
+            } 
+            string losujtag()
+            {
+                Random generator = new Random();
+                string tag = $"#{Convert.ToString((generator.Next(10000, 99999)))}";
+                return tag;
+
+            }
+
 
 
 
@@ -36,15 +79,28 @@ namespace ConsoleApp1
                 {
                     Console.WriteLine($"Zalogowano pomyślnie");
 
+                    while(true==true)
+                    {
+                        Console.Clear();
+                        string nazwakonta = getnick(login, password) + gettag(login, password);
+                        Console.WriteLine($"Nick:{nazwakonta}");
+                        Console.WriteLine("Zmień nick -- 1");
+                        Console.WriteLine("Zmień tag -- 2");
+                        string aktyw = Console.ReadLine();
+                        if (aktyw == "1")
+                        {
+                            Console.Write("Podaj nowy nick:")
+                            string newnick = Console.ReadLine();
+                            settag(login, password, newnick);
 
-
-
-
-                    // dalsze przejście
-
-
-
-
+                        }
+                        else if (aktyw == "2")
+                        {
+                            Console.Write("Podaj nowy tag:")
+                            string newtag = Console.ReadLine();
+                            settag(login, password, newtag);
+                        }
+                    }
                 }
                 else
                 {
@@ -72,7 +128,7 @@ namespace ConsoleApp1
                 }
             }
 
-            void Rejestracja(string login, string password)
+            void Rejestracja(string login, string password, string nick)
             {
                 string query = "select count(*) from users WHERE login ='" + login + "'";
                 MySqlCommand cmd = new MySqlCommand(query, con);
@@ -82,6 +138,8 @@ namespace ConsoleApp1
                     string cmdText = "insert into users(login,haslo) values('" + login + "','" + password + "');";
                     MySqlCommand cmdd = new MySqlCommand(cmdText, con);
                     cmdd.ExecuteNonQuery();
+                    setnick(login, password, nick);
+                    settag(login, password, losujtag());  
                 }
                 else
                 {
@@ -94,26 +152,6 @@ namespace ConsoleApp1
 
            
 
-
-
-
-
-
-            void zlehasloo()
-            {
-                Console.WriteLine($"Podano złe hasło");
-                Console.WriteLine($"Zarejestruj się");
-                Console.WriteLine($"Podaj login nowego konta");
-                Console.Write($"Login:");
-                string login11 = Console.ReadLine();
-                Console.WriteLine($"Podaj hasło nowego konta");
-                Console.Write($"Hasło:");
-                string haslo11 = Console.ReadLine();
-                Console.WriteLine($"Podaj klucz bezpieczeństwa administratora aby utworzyć nowe konto");
-                Console.Write($"Klucz:");
-                string kbez = Console.ReadLine();
-                Console.WriteLine();
-            }
 
 
 
@@ -151,12 +189,28 @@ namespace ConsoleApp1
 
 
 
-       
 
 
 
 
-            while(true == true)
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            while (true == true)
             {
                 Console.Clear();
                 Console.WriteLine($"Logowanie -- 1");
@@ -184,9 +238,12 @@ namespace ConsoleApp1
                     Console.WriteLine($"Podaj powtórzone hasło");
                     Console.Write($"Powtórzone hasło:");
                     string phaslo = Console.ReadLine();
+                    Console.WriteLine($"Podaj Nick");
+                    Console.Write($"Nick:");
+                    string nick = Console.ReadLine();
                     if (phaslo == haslo)
                     {
-                        Rejestracja(login, haslo);
+                        Rejestracja(login, haslo, nick);
                     }
                     else
                     {
